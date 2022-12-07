8ebbf8ae715bda54bb29a574d441e3a35f056d11
aron-thor-haraldsson <aron.thor.haraldsson@gmail.com>
2022-12-07 21:03:00 +0000

    wait, find cookie accept and click

--- scrape_site.py
+++ scrape_site.py
@@ -1,5 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 
 PATH = r'C:\Program Files (x86)\Google\Chrome\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
 driver.get("https://www.cfcunderwriting.com")
+
+WebDriverWait(driver, 5)
+search = driver.find_element(By.ID, "ccc-notify-accept")
+search.click()
+WebDriverWait(driver, 5)
